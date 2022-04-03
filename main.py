766060d6ab1b9b0bb97f7affd1ff0a725c6e0d8a
wanjira-whitney <wanjirawhitney@gmail.com>
2022-04-03 19:52:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+adj = input("Adjective: ")
+verb1 = input ("Verb: ")
+verb2 = input ("Verb: ")
+famousperson = input("Famous person: ")
+
+wanji = "Computer programming is so {adj}! It makes me so excited all \
+the time because I love to {verb1}. Stay hydrated and {verb2} like you are \
+{famousperson}".format(adj=adj, verb1=verb1, verb2=verb2, famousperson=famousperson)
+
+print(wanji)
+
+
